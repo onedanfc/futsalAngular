26c8da713afc7deeeaeae03a5310e5d81aad9176
onedanfc <onedanfc@gmail.com>
2024-02-08 19:20:44 +0700

    form login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'futsalAngular';
+  title = 'futsal Reserfation System';
 }
